825e9e20ddbbd4973d6e8239970ce75522ecbd0f
YvetteBal <Yvette_balanski@hotmail.com>
2023-12-20 16:03:38 +1300

    Triangle

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,4 +1,8 @@
 import java.awt.Color;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
    import java.awt.event.ActionEvent;
    import java.awt.event.ActionListener;
    import java.awt.event.KeyEvent;
@@ -10,7 +14,9 @@ import java.awt.Color;
 	   public PongPanel() {
 		   
 		      setBackground(BACKGROUND_COLOUR);
-		  
+		      Timer timer = new Timer(TIMER_DELAY, this);
+
+		       timer.start();
 		  }
       @Override
       public void keyPressed(KeyEvent event) {
@@ -26,9 +32,17 @@ import java.awt.Color;
   
       @Override
       public void actionPerformed(ActionEvent event) {
+    	  update();
       }
+      @Override
+      public void paintComponent(Graphics g) {
+       super.paintComponent(g);
+       g.setColor(Color.WHITE);
+      g.fillRect(20, 20, 100, 100);
+       }
   
       private final static Color BACKGROUND_COLOUR = Color.BLACK;
+      private final static int TIMERDELAY = 5;
 	
 
     }
